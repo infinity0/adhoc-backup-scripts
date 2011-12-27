46b5cd5ed1c1ba6a2a6c611a3dd7d65123d6811c
Ximin Luo <infinity0@gmx.com>
2011-12-27 13:55:17 +0000

    restructure to not need so many files, which was confusing. also general code cleanup.

--- archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-{
-	cat archive.txt
-} | ./backup-util make_backup_points
--- current.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-{
-	cat current.txt
-	cat current-cfg.txt | ./backup-util find_updated_configs;
-} | ./backup-util make_backup_points
